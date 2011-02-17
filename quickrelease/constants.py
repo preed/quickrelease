70813cfbedd056a3668688ca5ce494b091803587
J. Paul Reed <jpreed@gmail.com>
2011-02-17 14:32:37 -0800

    Remove SB_ prefix from constants.

--- quickrelease/constants.py
+++ quickrelease/constants.py
@@ -8,37 +8,37 @@ QUICKRELEASE_CONSTANTS = {
    'UNZIP' : 'unzip',
    'WGET' : 'wget',
    
-   'SB_BUILD_PLATFORMS': ('windows-i686-msvc8',
-                          'macosx-i686',
-                          'linux-i686',
-                          'linux-x86_64'),
+   'BUILD_PLATFORMS': ('windows-i686-msvc8',
+                       'macosx-i686',
+                       'linux-i686',
+                       'linux-x86_64'),
 
-   'SB_BUILD_PLATFORM_EXTENSIONS': { 'windows-i686-msvc8': 'exe',
-                                     'macosx-i686': 'dmg',
-                                     'linux-i686': 'tar.gz',
-                                     'linux-x86_64': 'tar.gz' },
+   'BUILD_PLATFORM_EXTENSIONS': { 'windows-i686-msvc8': 'exe',
+                                  'macosx-i686': 'dmg',
+                                  'linux-i686': 'tar.gz',
+                                  'linux-x86_64': 'tar.gz' },
 
    # in seconds, so five minutes
    'RUN_SHELL_COMMAND_DEFAULT_TIMEOUT': 60 * 5,
 
    # in seconds, so 10 mintues.
-   'SB_S3_PUSH_TIMEOUT': 60 * 10,
+   'S3_PUSH_TIMEOUT': 60 * 10,
 
-   'SB_S3_MIME_TYPES': { 'asc' : 'text/plain',
-                         'bz2' : 'application/x-bzip2',
-                         'dmg' : 'application/x-apple-diskimage',
-                         'exe' : 'application/octet-stream',
-                         'mar' : 'application/octet-stream',
-                         'md5' : 'text/plain',
-                         'tar.gz' : 'application/x-gzip',
-                         'txt': 'text/plain',
-                         'zip': 'application/zip',
-                       },
+   'S3_MIME_TYPES': { 'asc' : 'text/plain',
+                      'bz2' : 'application/x-bzip2',
+                      'dmg' : 'application/x-apple-diskimage',
+                      'exe' : 'application/octet-stream',
+                      'mar' : 'application/octet-stream',
+                      'md5' : 'text/plain',
+                      'tar.gz' : 'application/x-gzip',
+                      'txt': 'text/plain',
+                      'zip': 'application/zip',
+                    },
 }
 
 CONSTANTS_FROM_ENV_HANDLERS = {
-   'SB_BUILD_PLATFORMS': lambda val: tuple(val.split()),
-   'SB_BUILD_PLATFORM_EXTENSIONS': lambda val: NotImplementedError("Need to turn SB_BUILD_PLATFORM_EXTENSIONS overloads into a dict!"), 
-   'SB_BUILD_S3_MIME_TYPES': lambda val: NotImplementedError("Need to turn SB_BUILD_S3_MIME_TYPES overloads into a dict!"), 
+   'BUILD_PLATFORMS': lambda val: tuple(val.split()),
+   'BUILD_PLATFORM_EXTENSIONS': lambda val: NotImplementedError("Need to turn BUILD_PLATFORM_EXTENSIONS overloads into a dict!"), 
+   'S3_MIME_TYPES': lambda val: NotImplementedError("Need to turn S3_MIME_TYPES overloads into a dict!"), 
 }
 
