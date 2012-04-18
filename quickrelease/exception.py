2c11145b8b4db0bbde9136180862674fb41e25dc
J. Paul Reed <jpreed@gmail.com>
2012-04-18 02:03:54 -0700

    Initial documentation for the quickrelease.exception classes.

--- quickrelease/exception.py
+++ quickrelease/exception.py
@@ -1,8 +1,25 @@
 # ex:ts=4:sw=4:sts=4:et
 # -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
+r"""Various exception/error types used by both QuickRelease and available for 
+QuickRelease users to use in their own code.
+"""
+
+## TODO: separate out QuickRelease-specific errors from ones for users.
+
 class ReleaseFrameworkError(Exception):
+    """
+    A standard error class to indicate a error with the release framework.
+    """
     def __init__(self, explanation, details=None):
+        """
+        Construct the ReleaseFrameworkError.
+
+        @param explanation: A summary of the exception.
+        @type explanation: C{str} (or covertable to)
+
+        @param details: Further details regarding the exception.
+        """
         self._explanation = explanation
         self._details = details
         Exception.__init__(self, explanation)
@@ -11,14 +28,30 @@ class ReleaseFrameworkError(Exception):
     def _GetDetails(self): return self._details
 
     explanation = property(_GetExplanation)
+    """A short summary of the exception. Read-only.
+    @type: C{str}"""
     details = property(_GetDetails)
+    """Extended details, if provided, regarding the exception. Read-only."""
 
     def __str__(self):
         return str(self.explanation)
 
+## TODO: provide an __iter__ method for this
 class ReleaseFrameworkErrorCollection(ReleaseFrameworkError):
-    def __init__(self, group):
-        # copy?
+    """
+    A collection class to collect multiple ReleaseFrameworkErrors and handle
+    them as a group. This class is useful when you've got a repeated process
+    (e.g. partner processes), but want to attempt all the process with all the
+    partners before halting.
+    """
+    def __init__(self, group=()):
+        """
+        Create the ReleaseFrameworkErrorCollection.
+
+        @param group: An existing C{list} of r{ReleaseFrameworkError}s.
+        @type group: C{list}
+        """
+        # TODO: deepcopy these
         self._errorGroup = list(group)
         ReleaseFrameworkError.__init__(self, None, group)
 
@@ -29,5 +62,11 @@ class ReleaseFrameworkErrorCollection(ReleaseFrameworkError):
         return "Release Framework Errors:\n\t%s" % '\n\t'.join(errorStrs)
 
     def append(self, releaseFrameworkError):
+        """Attach the specific L{ReleaseFrameworkError} to the collection.
+
+        @param releaseFrameworkError: The L{ReleaseFrameworkError} to be added 
+        to the collection.
+        @type releaseFrameworkError: L{ReleaseFrameworkError}
+        """
         self._errorGroup.append(releaseFrameworkError)
         self._explanation = str(self)
