c2d1053b0cf15914f7ee8d719326b85d293145fa
J. Paul Reed <jpreed@gmail.com>
2012-04-17 23:49:32 -0700

    Initial documentation for the quickrelease.ConfigSpec/ConfigSpecError classes

--- quickrelease/config.py
+++ quickrelease/config.py
@@ -1,6 +1,21 @@
 # ex:ts=4:sw=4:sts=4:et
 # -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
+r"""quickrelease.config - A representation of a key/value configuration 
+specification, selected by the user at runtime.
+
+This module is wrapper around the builtin ConfigParser module. It removes the 
+ability to modify the configuration at runtime, and adds some 
+quickrelease-specific functionality (definition of "partners" and
+"deliverables.")
+
+It also provides methods for retriving defined constants, which may be
+overridden in various ways.
+
+The format of the configuration specification is described in the blah blah 
+module documentation.
+"""
+
 import ConfigParser
 import os
 import re
@@ -9,12 +24,28 @@ from quickrelease import constants
 from quickrelease.exception import ReleaseFrameworkError
 
 class ConfigSpecError(ReleaseFrameworkError):
+    """
+    An exception class representing various errors that can occur with 
+    L{ConfigSpec}s.
+
+    It also re-expresses many of 
+    L{SafeConfigParser<ConfigParser.SafeConfigParser>}'s exceptions as 
+    constants which can be tested for by querying the C{details} property of 
+    the exception.
+    """
+
     NO_OPTION_ERROR = 0
+    """See L{ConfigParser.NoOptionError}"""
     INTERPOLATION_MISSING_OPTION_ERROR = 1
+    """See L{ConfigParser.InterpolationMissingOptionError}"""
     INTERPOLATION_SYNTAX_ERROR = 2
+    """See L{ConfigParser.InterpolationSyntaxError}"""
     COERCION_TYPE_ERROR = 3
+    """The type requested for a coercion was a non-supported type."""
     NO_SECTION_ERROR = 4
+    """See L{ConfigParser.NoSectionError}"""
     PARSE_ERROR = 5
+    """See L{ConfigParser.ParsingError}"""
 
     def __init__(self, errorStr, details=None):
          ReleaseFrameworkError.__init__(self, errorStr, details)
@@ -27,6 +58,10 @@ OVERRIDES_DISABLED_ERR_STR = ("Commandline variable overrides are not enabled "
  "default section.")
 
 class ConfigSpec(object):
+    """
+    A class representing a QuickRelease runtime-specified configuration 
+    specification for a process.
+    """
     DEFAULT_SECTION = 'DEFAULT'
     CONFIG_SECTION_DELIMETER = ':'
     DELIV_SECTION_PREFIX = 'deliverable'
@@ -34,6 +69,31 @@ class ConfigSpec(object):
 
     @staticmethod
     def GetConstant(name):
+        """
+        Retrive the named constant from the environment, if it's defined there,
+        or from L{quickrelease.constants}. The return type can be a complex
+        Python type.
+
+        This provides a way to retrieve some configuration values without
+        having direct access to a L{ConfigSpec} handle.
+
+        It also allows those values to be overridden by setting environment
+        variables.
+
+        For this reason, it is generally preferable to use instantiated 
+        L{ConfigSpec} handles instead of this method. However, this method can
+        be (and is) used in places where it's not convenient to obtain one.
+
+        @param  name: The name of the constant to retrieve.
+        @type   name: C{str}
+
+        @returns: The value of the requested constant.
+        @rtype:   Mixed, depending on how the variable was defined in 
+                  L{quickrelease.constants}, or whether a converter was 
+                  specified for the variable when obtaining it from the 
+                  environment.
+        """
+
         value = os.getenv(name)
 
         if value is not None:
@@ -49,10 +109,53 @@ class ConfigSpec(object):
 
     @staticmethod
     def GetDefinedConstants():
+        """
+        @returns: A list of all defined constants. (Does not include constants 
+                  available via the environment.)
+        @rtype:   C{list}
+        """
         return constants.QUICKRELEASE_CONSTANTS.keys()
 
     def __init__(self, configFile, rootDir=os.getcwd(),
      section=DEFAULT_SECTION, overrides=()):
+        """
+        Create a handle to a QuickRelease configuration specification
+        ("C{ConfigSpec}") for use.
+
+        @param configFile: The ConfigPaser .ini-style file to parse
+        @type  configFile: C{str}
+
+        @param rootDir: The "root directory" to use for this config;
+        defaults to the current working directory at the time the ConfigSpec is 
+        initialized. Is generally used to specify a directory 
+        @type  rootDir: C{str}
+
+        @param section: Section to start in. Defaults to the ini file's
+        "default" section.
+        @type  section: C{str}
+
+        @param overrides: A list of strings representing overrides to apply
+        to the given config specification. The expected format for each string
+        is: "section:key=value" Section may be omitted, and in this case, the 
+        override is added to the default section.
+
+        B{Note}: the allow_config_overrides item B{must} be set in the config 
+        file, or an exception will be raised if any are provided. 
+        @type  overrides: C{list} of C{str}
+
+        @return:  C{ConfigSpec} reference
+
+        @raise ConfigSpecError: raises C{ConfigSpecError}s exceptions in the
+        following cases:
+            1. The specified config file to parse is missing or an invalid value
+            was specified.
+            2. The initial section specifed doesn't exist in the config
+            3. Overriding of values is not allowed by the config, but overrides
+            were provided.
+            4. L{SafeConfigParser<ConfigParser.SafeConfigParser>} errors 
+            encountered while parsing the config file may be raised as 
+            converted L{ConfigSpecError<quickrelease.config.ConfigSpecError>}s.
+        """
 
         # We have to error check this ourselves because the ConfigParser class
         # doesn't (this is a feature!)
@@ -155,6 +258,18 @@ class ConfigSpec(object):
         self._currentSection = newSection
 
     def GetSectionItems(self, sectionName=None):
+        """
+        @param sectionName: the section to return the list of defined items for;
+        defaults to the currently-selected section.
+        @type  sectionName: C{str}
+
+        @return:  A list of defined items ("options") in the specified section.
+        @rtype:   C{list} of C{str}
+
+        @raise ConfigSpecError: L{SafeConfigParser<ConfigParser.SafeConfigParser>} errors are converted 
+        to L{ConfigSpecError<quickrelease.config.ConfigSpecError>} s as 
+        appropriate.
+        """
         if sectionName is None:
             sectionName = self.section
 
@@ -165,6 +280,19 @@ class ConfigSpec(object):
             raise ConfigSpec._ConvertToConfigParserError(ex)
 
     def GetSectionElements(self, sectionName=None):
+        """
+        @param sectionName: the section to return the list of defined elements
+        for; defaults to the currently-selected section.
+        @type  sectionName: C{str}
+
+        @return:  A list of defined items ("options") in the specified section.
+        @rtype:   C{list} of two-element C{list}: (C{item}, C{value})
+
+        @raise ConfigSpecError: L{SafeConfigParser<ConfigParser.SafeConfigParser>} errors are converted 
+        to L{ConfigSpecError<quickrelease.config.ConfigSpecError>}s as 
+        appropriate.
+        """
+
         if sectionName is None:
             sectionName = self.section
 
@@ -173,14 +301,38 @@ class ConfigSpec(object):
             return self.rawConfig.items(sectionName)
         except ConfigParser.Error, ex:
             raise ConfigSpec._ConvertToConfigParserError(ex)
-                             
+
     rootDir = property(_GetRootDir)
+    """ The root directory set when the configuration specification object was
+    created. Read-only.
+    @type: C{str}"""
+
     configFile = property(_GetConfigFile)
+    """ The name of the config file loaded when the configuration specification
+    object was created. Read-only.
+    @type: C{str}"""
+
     rawConfig = property(_GetRawConfig)
+    """The underlying L{SafeConfigParser<ConfigParser.SafeConfigParser>}
+    handle. Read-only.
+    @type: L{SafeConfigParser<ConfigParser.SafeConfigParser>}"""
+
     section = property(_GetSection, _SetSection)
+    """The current section name.
+    @type: C{str}"""
+
     sectionList = property(_GetSectionList)
+    """A C{list} of all defined sections in the configuration specification.
+    Read-only.
+    @type: C{list}"""
     sectionItems = property(GetSectionItems)
+    """A C{list} of all defined items ("options") in the current section.
+    Read-only.
+    @type: C{list}"""
     sectionElements = property(GetSectionElements)
+    """A C{list} of (option, value) lists defined in the current section.
+    Read-only.
+    @type: C{list}"""
 
     def _ResetPartnerDefaultSectionVars(self):
         for key in self.rawConfig.defaults().keys():
@@ -191,6 +343,24 @@ class ConfigSpec(object):
         # pprint.pprint(self.rawConfig.defaults())
 
     def SetPartnerSection(self, partner):
+        """
+        Set the C{ConfigSpec}'s current section to that of the named partner.
+
+        This method will also update the default section so all 
+        partner-specific variables may be used in other sections by prefacing
+        them with C{PARTNER_}.
+        
+        Example: a section on version numbers includes different suffixes
+        for partners. Each partner defines its unique C{version_number_suffix} 
+        in its own section. Then, any common section can reference 
+        C{PARTNER_version_number_suffix} and the appropriate values will be 
+        substituted.
+
+        @param partner: The name of the partner's partner-specific section 
+        to switch to.
+        @type  partner: C{str}
+        """
+
         if not self.ValidPartner(partner):
             raise ConfigSpecError("Invalid/unknown partner: %s" % (partner))
 
@@ -227,19 +397,71 @@ class ConfigSpec(object):
          ConfigSpec.CONFIG_SECTION_DELIMETER + delivName)
 
     def ValidDeliverable(self, deliverable):
+        """
+        Determine whether the given deliverable name is "valid," e.g.
+        defined in the L{ConfigSpec} provided by the user.
+
+        @type  deliverable: C{str}
+        @param deliverable: The name of the deliverable to validate.
+
+        @rtype:   C{bool}
+        @return:  Does the given string represent a validly defined
+        "deliverable" for the current configuration specification.
+        """
         return (self._GetDeliverableSectionName(deliverable) in
          self.sectionList)
 
     def ValidPartner(self, partner):
+        """
+        Determine whether the given partner name is "valid," e.g. defined in the
+        L{ConfigSpec} provided by the user.
+
+        @param partner: The name of the partner to validate.
+        @type  partner: C{str}
+
+        @rtype:   C{bool}
+        @return:  Does the given string represent a validly defined "partner"
+        for the current configuration specification.
+        """
         return self._GetPartnerSectionName(partner) in self.sectionList
 
     def PartnerGet(self, partner, name, coercion=None, interpolation={}):
+        """
+        Retrieve the value for the given item name in the specified-partner's
+        section.
+
+        Care is taken to ensure that the C{ConfigSpec} handle state is left as
+        it was.
+
+        @param partner: The partner to query the partner-section for the
+        requested item. 
+        @type  partner: C{str}
+
+        @see: L{ConfigSpec.Get()<quickrelease.config.ConfigSpec.Get>} for a 
+        comprehensive explanation of this method's other arguments and 
+        possible exceptions.
+        """
+        # TODO: this really needs to call SetPartnerSection() for fully-correct
+        # behavior.
         return self.SectionGet(self._GetPartnerSectionName(partner),
                                       name,
                                       coercion,
                                       interpolation)
 
     def SectionGet(self, section, name, coercion=None, interpOverrides={}):
+        """
+        Retrieve the value for the given item name in the specified-section.
+        Care is taken to ensure that the C{ConfigSpec} handle state is left as
+        it was.
+
+        @param section: The name of the section to obtain the requested item
+        from.
+        @type  section: C{str}
+
+        @see: L{ConfigSpec.Get()<quickrelease.config.ConfigSpec.Get>} for a 
+        comprehensive explanation of this method's other arguments and 
+        possible exceptions.
+        """
         origSection = self.section
 
         try:
@@ -250,7 +472,39 @@ class ConfigSpec(object):
 
         return value
 
+    # TODO: when the default section is specified, restrict access to just
+    # what defaults() returns
     def Get(self, name, coercion=None, interpOverrides={}):
+        """
+        Retrieve the value for the given item name in the current section.
+
+        @param name: Name of the option to retrieve.
+        @type  name: C{str}
+
+        @param coercion: A Python type instance to coerce the return into.
+        Defined coercions exist for C{bool}, C{str}, C{int}, C{float}, C{list},
+        and C{dict}; by default, a C{str} is returned.
+        @type  coercion: C{type}
+
+        @param interOverrides: If overrides are allowed, a dictionary of 
+        key/value pairs to override values for for the current request. If set i
+        to None, no L{SafeConfigParser<ConfigParser.SafeConfigParser>}
+        interpolation will take place, and the raw values specified in the 
+        configuration specification will be returned.
+        @type  interpOverrides: C{dict} of key-value pairs to override or 
+        C{None}
+
+        @rtype:  C{str}, unless a coercion is specified.
+        @return: The option specified in the config specification for the 
+        given name in the current section.
+
+        @raise ConfigSpecError: 
+        L{SafeConfigParser<ConfigParser.SafeConfigParser>} errors are converted 
+        to L{ConfigSpecError<quickrelease.config.ConfigSpecError>}s as 
+        appropriate.
+        """
+       
+
         getRawValues = interpOverrides is None
         overrides = None
 
